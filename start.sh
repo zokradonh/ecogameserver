59085b0e3fe1b98226bc2dbc198d1f85cde0908a
Andre Zoledziowski <az@zok.xyz>
2019-03-06 14:00:54 +0100

    SIGTERM is now supported by ECO

--- start.sh
+++ start.sh
@@ -3,26 +3,4 @@
 # populate with missing default config files 
 cp -n /app/DefaultConfigs/* /app/Configs/
 
-pipe=$(mktemp -u)
-mkfifo $pipe
-
-# trap the SIGTERM
-trap "printf \"exit\n\" >> $pipe" SIGTERM
-
-# start server
-cd /app
-cat $pipe | mono /app/EcoServer.exe -nogui &
-PID=$!
-# wait for trap or exit
-wait $PID
-# wait for graceful exit (saving world)
-START=$(date +%s)
-wait $PID
-EXIT_STATUS=$?
-END=$(date +%s)
-DIFF=$(($END - $START))
-
-echo "Server gracefully shutdown in $DIFF seconds."
-
-rm $pipe
-exit $EXIT_STATUS
+exec mono /app/EcoServer.exe -nogui
